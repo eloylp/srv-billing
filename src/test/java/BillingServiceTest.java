dc7ada8e56239dfbcc9ab8ab8c8738734ffca721
eloylp <eloy@sandboxwebs.com>
2017-01-14 02:49:55 +0100

    Added junit anf first test.

---
+++ src/test/java/BillingServiceTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BillingServiceTest {
+
+    @Test
+    public void assertSeriesNeededMatching() {
+
+
+        //assertEquals();
+
+
+    }
+}
