b8244bb93b660baed81b904d621a709adb917341
eloylp <eloylpdev@gmail.com>
2019-04-07 18:28:43 +0200

    removing unnecessary framework annotations.

--- src/main/java/billing/domain/Invoice.java
+++ src/main/java/billing/domain/Invoice.java
@@ -1,15 +1,11 @@
 package billing.domain;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 import java.util.List;
 
-@Document
 public class Invoice {
 
-    @Id
     private String id;
     private String delegationName;
 
